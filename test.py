542aed8d6bdd9f71e38d375c79bf8d179323dde8
Lakshita Pagaria <iamlakshita31@gmail.com>
2025-12-03 21:04:31 +0530

    Change the code

--- test.py
+++ test.py
@@ -1,11 +1,2 @@
-def multiplication():
-    print(5*4*3)
-
-def greet():
-    print("Hello")
-
-def add():
-    print(3+2+3)
-
-def sub():
-    print(10-4-1)
+def greetings():
+    print("Hello, I am Lakshita Pagaria!")
